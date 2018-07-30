8ee4d238033361b0c2f3734d94db7b7b0924351c
Adam Modras <25adam@gmail.com>
2018-07-30 11:02:38 -0700

    Adding loader files

---
+++ app/src/components/loaders/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loader.css';
+
+export default () => {
+  return (
+    <div>
+      <div className="loader">
+        <span>{'{'}</span> <span>{'}'}</span>
+      </div>
+    </div>
+  )
+}
