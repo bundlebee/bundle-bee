53a0577b255673bf02a2828f34bed89e8ffed003
Brendan Morrell <brendanmorrell@gmail.com>
2018-08-02 12:54:40 -0700

    last revert file

--- app/src/redux/actions/dataActions.js
+++ app/src/redux/actions/dataActions.js
@@ -3,11 +3,10 @@ import * as d3 from 'd3';
 
 export const retrieveCompilationStats = () => {
   return function(dispatch) {
-    console.log('right here');
-
-    d3.json('../../stats.json')
+    d3.json('/Users/bren/Desktop/stats.json')
       .then(function(data) {
         console.log(data);
+
         // what action type should we dispatch?
         dispatch({ type: types.BUNDLE_COMPLETE, payload: data });
       })
