061ebd609e56a7695e3b3c26ad6a1351dd076221
Brendan Morrell <brendanmorrell@gmail.com>
2018-08-06 13:28:22 -0700

    added electronUserData folder to be saved but with its own gitignore that will ignore all files inside the folder. also added a functionin the file indexer that tries to parse the entry from the webpack config and uses that if it parses an existing file from the entry

--- backend/create-config/utils/indexFilesFromRoot.js
+++ backend/create-config/utils/indexFilesFromRoot.js
@@ -1,14 +1,17 @@
 const returnCurrentDirectoryFromPath = require('./indexFilesFromRootHelpers/returnCurrentDirectoryFromPath.js');
 const getAllFilesInCurrentDirectory = require('./indexFilesFromRootHelpers/getAllFilesInCurrentDirectory.js');
 const getInfoForWebpackConfigFromFileList = require('./createWebPackConfigHelpers/getInfoForWebpackConfigFromFileList.js');
+const tryAndSetEntryFromWebpackConfigEntry = require('./createWebPackConfigHelpers/parseEntryFromWebpack.js');
 const writeToFile = require('./file-and-system-actions/writeToFile.js');
 
 const pathFromDrag = process.argv[process.argv.length - 1];
 
 returnCurrentDirectoryFromPath(pathFromDrag)
   .then(rootDir => getAllFilesInCurrentDirectory(rootDir))
-  .then(files => getInfoForWebpackConfigFromFileList(files))
+  .then(res => getInfoForWebpackConfigFromFileList(res))
+  .then(res => tryAndSetEntryFromWebpackConfigEntry(res, res.webpackConfig))
   .then(res => writeToFile(res, 'configurationData.js'))
+  // .then(res => console.log(res))
   .then(res => {
     process.send({
       foundWebpackConfig: res.webpackConfig.exists,
