9e174dc461e38a097d4a3dd473408904a1092567
Clariz Mariano <clariz.mariano@gmail.com>
2018-08-06 14:46:01 -0700

    cleaned up for PR

--- app/src/components/data_viz/helper_components/displayButton.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
- 
-const displayButton = (props) => (
-  <button onClick={props.handleClick} className={props.isHighligthed ? d3_btn_highligthed : d3_btn_default}
-  ></button>    
-)
