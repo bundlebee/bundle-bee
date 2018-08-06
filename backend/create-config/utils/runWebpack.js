7546c1e79a21fb5e825e746ec0bacee209ce05ee
Brendan Morrell <brendanmorrell@gmail.com>
2018-08-06 14:10:26 -0700

    fixed windows bug by changing from child_process spawn to child_process exec in runWebpack.js

--- backend/create-config/utils/runWebpack.js
+++ backend/create-config/utils/runWebpack.js
@@ -1,20 +1,39 @@
 const fs = require('fs');
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 
 const statsWritePath = process.argv[process.argv.length - 1];
-const resultsStream = fs.createWriteStream(statsWritePath);
+// const resultsStream = fs.createWriteStream(statsWritePath);
 
-const child = spawn('webpack', ['--profile', '--json']);
-
-child.stdout.pipe(resultsStream);
-
-child.on('close', function(code) {
-  console.log('child process exited with code ' + code);
-  process.send('done');
-  process.exit();
+exec(`webpack --profile --json > ${statsWritePath}`, null, error => {
+  if (error) process.send({ error });
+  else process.send({ status: 'done' });
 });
 
-child.on('error', error => {
-  process.send({ error });
-  process.exit();
-});
+// exec(`webpack --profile --json`, { maxBuffer: 1024 * 100000 }, (error, stdout) => {
+//   if (error) {
+//     process.send({ error });
+//   } else {
+//     fs.writeFile('/Users/bren/Desktop/stats.json', stdout, (error, res) => {
+//       if (error) {
+//         process.send({ error });
+//       } else {
+//         process.send({ webpack: 'complete' });
+//       }
+//     });
+//   }
+// });
+
+// child.stdout.pipe(resultsStream);
+
+// child.on('close', function(code) {
+//   setTimeout(() => {
+//     console.log('child process exited with code ' + code);
+//     process.send('done');
+//     process.exit();
+//   }, 5000);
+// });
+
+// child.on('error', error => {
+//   process.send({ error });
+//   process.exit();
+// });
