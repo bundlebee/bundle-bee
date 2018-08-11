7c4fe000478f75239dffa90ac0958c44e47fba90
Sam Goldberg <sgoldber61@gmail.com>
2018-08-11 14:21:51 -0700

    rollup displays on front end

--- app/src/redux/reducers/dataReducer.js
+++ app/src/redux/reducers/dataReducer.js
@@ -10,14 +10,16 @@ const dataReducer = (state = initialState, action) => {
   // console.log(state, "SB DATA~~~~")
   switch (action.type) {
     case types.BUNDLE_WEBPACK_COMPLETE: {
+      console.log('bundle webpack complete');
       return { ... state, webpackStarBurstData: action.payload };
    }
     case types.BUNDLE_PARCEL_COMPLETE: {
       console.log('bundle parcel complete');
-      console.log(action.payload);
       return {... state,  parcelStarBurstData: action.payload };
  }   
     case types.BUNDLE_ROLLUP_COMPLETE: {
+      console.log('bundle rollup complete');
+      console.log(action.payload);
        return {... state,  rollupStarBurstData: action.payload };
 }   
     default: {
