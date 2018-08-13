8564d32549778993b66358004c90fb2a5f6bfcb7
Brendan Morrell <brendanmorrell@gmail.com>
2018-08-13 14:26:38 -0700

    got rollup bundler stats working and writing files to rollup-dist

--- backend/create-config/utils/rollupConfigHelpers/rollupBundler.js
+++ backend/create-config/utils/rollupConfigHelpers/rollupBundler.js
@@ -4,18 +4,23 @@ const path = require('path');
 
 const rollupConfigPath = process.argv[2];
 const config = require(rollupConfigPath);
-const inputOptions = {input: config.input, plugins: config.plugins};
+const inputOptions = { input: config.input, plugins: config.plugins };
 const outputOptions = config.output;
 
 const beginTime = Date.now();
-
-rollup.rollup(inputOptions).then(bundle => {
-  return bundle.generate(outputOptions);
-}).then(result => {
-  const totalElapsedTime = Date.now() - beginTime;
-  const totalBundleSize = Buffer.from(result.code).length;
-  
-  fs.writeFileSync(path.join(__dirname, '..', '..', '..', '..', 'electronUserData', 'rollup-totals-stats.json'), JSON.stringify({totalBundleSize, totalElapsedTime}, null, 2));
-});
-
-
+let Bundle;
+rollup
+  .rollup(inputOptions)
+  .then(bundle => {
+    Bundle = bundle;
+    return bundle.generate(outputOptions);
+  })
+  .then(result => {
+    const totalElapsedTime = Date.now() - beginTime;
+    const totalBundleSize = Buffer.from(result.code).length;
+    fs.writeFileSync(
+      path.join(__dirname, '..', '..', '..', '..', 'electronUserData', 'rollup-totals-stats.json'),
+      JSON.stringify({ totalBundleSize, totalElapsedTime }, null, 2)
+    );
+    Bundle.write(outputOptions);
+  });
